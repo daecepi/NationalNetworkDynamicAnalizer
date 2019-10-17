d0ab493847781a1c48cbb68c7f6d06bf7254a9eb
David E Cermeno Pizon <daecepi@gmail.com>
2019-10-17 09:22:45 -0500

    Algorithm complemented with the degmat matrix cal

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ _By: David Eduardo Cermeño Pinzon_
 ## Description
 Using C to speed up the execution of Colombia's electrical network's analisys of its dinamics
 
-## Dependencies
-- 
+## Requirements by the project director
+That it could be a single file script that he could run (in a monolithic coding style)
 
-#### By Universidad de Cartagena in its adscrited internal project
+## Dependencies
+Internal libraries
+- stdio
+- stdlib
+
+#### By Universidad de Cartagena in its adscrited internal project 
--- script.c
+++ script.c
@@ -9,24 +9,29 @@ const int N = 125;
 const int K_strength = 3;
 
 //FUNCTION PROTOTYPE'S SECTION
-int evaluateDegreeToCero(int[N][N]);
-
+double evaluateDegreeToCero(double[N][N]);
+double sumVector(double[N]);
 int main(){
+    double defaultWhite[N][N];
+    double K[N][N]; // matriz de conectividad
+    double degMat[N][N];
+
+    //Initialize with zeros
+    for(int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            defaultWhite[i][j] = 0;
+            K[i][j] = 0;
+            degMat[i][j] = 0;
+        }
+    }
+
     // p_conn: prbabilidad de conexión
     const float p_conn = 0.05;
 
     // Parámetro de inercia
     const float alpha = 0.1;
 
-    // Inicializo la matriz de conectividad tamaño NxN
-    int K[N][N]; 
-    
-    //Initialize with zeros
-    for(int i = 0; i < N; i++){
-        for (int j = 0; j < N; j++){
-            K[i][j] = 0;
-        }
-    }
+    // Inicializo la matriz de conectividad tamaño NxN and initialize with zeros
 
     int flag; //Variable that ressembles bool variables
 
@@ -46,12 +51,55 @@ int main(){
             }
         }
 
-        flag = evaluateDegreeToCero(K);
-        printf("%d\n",flag);
+        flag = evaluateDegreeToCero(K); // corrigiendo la flag que terminará el loop
     }
 
     flag = evaluateDegreeToCero(K);
-        printf("%d\n",flag);
+    printf("%d\n",flag);
+
+    //Este es el vector de "Potencias" generadas y consumidas por la red
+    double P[N];
+
+    //Initializing vector to zero
+    for (int i = 0; i < 5; i++){
+        P[i] = 10;
+    }
+    for (int i = 5; i < 25; i++){
+        P[i] = 2.5;
+    }
+    for (int i = 25; i < N; i++){
+        P[i] = -1;
+    }
+    
+
+    //La suma de P's debe ser igual a 0!
+    printf("\nThe sum must be cero and is: %f", sumVector(P));
+
+    double A[N][N];
+
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            A[i][j] = K[i][j]*K_strength;
+        }
+    }
+    
+    // DE AQUI EN ADELANTE SON CÁLCULOS NECESARIOS
+    // PARA EL PUNTO DE EQUILIBRIO
+
+    // 1. Defino el Laplaciano, para ello calculo primero el grado de cada nodo
+    // incluyendo su "fuerza de acople" y coloco estos valores en una matriz
+    // diagonal
+    //Aqui se usa 
+    for(int i = 0; i < N; i++){
+        double sum = 0.0;
+        for (int j = 0; j < N; j++)
+        {
+            sum = sum + A[i][j];
+        }
+        degMat[i][i] = sum;
+    }
+
+
     return 0;
 }
 
@@ -70,10 +118,9 @@ Function that initializes the each item
 /*
 Function that evals the node's degree on the matrix and returns 1 
 */
-int evaluateDegreeToCero(int matrix[N][N]){
-    int num = 0;
+double evaluateDegreeToCero(double matrix[N][N]){
     for(int i = 0; i < N ; i++){
-        int sum = 0;
+        double sum = 0.0;
         for (int j = 0; j < N; j++){
             
             sum = sum + matrix[i][j];
@@ -83,4 +130,15 @@ int evaluateDegreeToCero(int matrix[N][N]){
         }
     }
     return 0;
+}
+
+/*
+Function that returns that the sum of all the elements on the vector is zero
+*/
+double sumVector(double vec[N]){
+    double sum = 0.0;
+    for (int  i = 0; i < N; i++){
+        sum = sum + vec[i];
+    }
+    return sum;
 }
